22611cb53e68e9575fcbba202d503b1df9bfb722
vani <van973vv@gmail.com>
2024-06-08 23:18:02 +0800

    wtf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,12 @@
-function animateText() {
-  var element = document.querySelector(".typed");
-  var typed_strings = element.getAttribute("data-typed-items");
-  typed_strings = typed_strings.split(",");
-  new Typed("#dynamic-text", {
+function animateText(id, typeSpeed, backSpeed, backDelay, loop) {
+  const element = document.getElementById(id);
+  var typed_strings = element.getAttribute("data-typed-items").split(",");
+  new Typed(`#${id}`, {
     strings: typed_strings,
-    typeSpeed: 50,
-    backSpeed: 20,
-    backDelay: 1000,
-    loop: true,
-  });
-}
-
-function animateProgressBar() {
-  var progressBars = document.querySelectorAll(".progress .progress-bar");
-  new Waypoint({
-    element: document.querySelector(".skills-content"),
-    handler: function () {
-      progressBars.forEach(function (bar) {
-        var width = parseInt(bar.getAttribute("aria-valuenow"));
-        bar.style.width = width + "%";
-      });
-      this.destroy();
-    },
-    offset: "80%",
+    typeSpeed,
+    backSpeed,
+    backDelay,
+    loop,
   });
 }
 
@@ -30,45 +14,18 @@ function main() {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll("ul li a");
   const header = document.querySelector("header");
-  const menuIcon = document.querySelector("#menu-icon");
-  const lineButtons = document.querySelectorAll(".line-button");
-  animateText();
+  const openMenu = document.querySelectorAll(".menu-button");
+  animateText("dynamic-text", 50, 20, 1000, true);
 
-  var progressBarData = [
-    { label: "Full Stack Web Development", value: 94 },
-    { label: "Mobile Application Development", value: 78 },
-    { label: "Systems Programming", value: 83 },
-    { label: "UI/UX Design", value: 64 },
-    { label: "Game Development", value: 43 },
-    { label: "Data Science and Analytics", value: 67 },
-    { label: "Database Management", value: 83 },
-  ];
-
-  var progressBarsContainer = document.querySelector(".skills-content");
-  progressBarData.forEach(function (data, index) {
-    var delay = index * 100;
-    var progressBarHTML = `
-      <div class="progress" data-aos="fade-right" data-aos-delay="${delay}">
-        <span class="skill">${data.label} <i class="val">${data.value}%</i></span>
-        <div class="progress-bar-wrap">
-          <div class="progress-bar" role="progressbar" aria-valuenow="${data.value}" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-      </div>
-    `;
-    progressBarsContainer.insertAdjacentHTML("beforeend", progressBarHTML);
+  openMenu.forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".mobile-nav").classList.toggle("mobile-nav-open");
+    });
   });
-
-  animateProgressBar();
-
-  menuIcon.addEventListener("click", function () {
-    if (menuIcon.classList.contains("fa-bars")) {
-      menuIcon.classList.remove("fa-bars");
-      menuIcon.classList.add("fa-close");
-    } else {
-      menuIcon.classList.remove("fa-close");
-      menuIcon.classList.add("fa-bars");
-    }
-    header.classList.toggle("menu-open");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      document.querySelector(".mobile-nav").classList.toggle("mobile-nav-open");
+    });
   });
 
   window.addEventListener("scroll", () => {
@@ -92,18 +49,6 @@ function main() {
       if (link.getAttribute("href").substring(1) === current) {
         link.classList.add("active");
       }
-    });
-  });
-
-  lineButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      lineButtons.forEach((btn) => {
-        if (btn !== button) {
-          btn.classList.remove("active");
-        }
-      });
-
-      this.classList.toggle("active");
     });
   });
 }
