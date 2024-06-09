7441d44027b99a7c52006973a6ec9f4031d3a98b
vani <van973vv@gmail.com>
2024-06-09 10:21:18 +0800

    finalized

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,36 @@ function animateText(id, typeSpeed, backSpeed, backDelay, loop) {
   });
 }
 
+function updatePositions() {
+  const h1Elements = document.querySelectorAll("h1.heading-l.title");
+  const lineElement = document.querySelector(".line");
+  const existingCircles = document.querySelectorAll(".circle");
+  existingCircles.forEach((circle) => circle.remove());
+  if (h1Elements.length > 0) {
+    const firstH1Element = h1Elements[0];
+    const firstH1Position = firstH1Element.getBoundingClientRect();
+    const newTopPosition = firstH1Position.top + window.scrollY + 20;
+
+    lineElement.style.top = `${newTopPosition}px`;
+
+    h1Elements.forEach((h1Element) => {
+      const h1Position = h1Element.getBoundingClientRect();
+      const circleTopPosition = h1Position.top + window.scrollY;
+      const circle = document.createElement("div");
+      circle.className = "circle";
+      circle.style.top = `${circleTopPosition + 20}px`;
+      document.body.appendChild(circle);
+    });
+  }
+}
+
 function main() {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll("ul li a");
   const header = document.querySelector("header");
   const openMenu = document.querySelectorAll(".menu-button");
   animateText("dynamic-text", 50, 20, 1000, true);
+  updatePositions();
 
   openMenu.forEach((button) => {
     button.addEventListener("click", function () {
@@ -51,5 +75,7 @@ function main() {
       }
     });
   });
+
+  window.addEventListener("resize", updatePositions);
 }
 document.addEventListener("DOMContentLoaded", main());
